ff71f62011a4df64823a307fe936ab0c98e4d614
marwan191289-eng <marwan191289@gmail.com>
2026-04-22 18:04:21 +0400

    Update app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { type Express } from "express";
 import cors from "cors";
-import pinoHttp from "pino-http";
+import { pinoHttp } from "pino-http"; // ✔ FIXED
 import router from "./routes";
 import { logger } from "./lib/logger";
 
@@ -25,6 +25,7 @@ app.use(
     },
   }),
 );
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
